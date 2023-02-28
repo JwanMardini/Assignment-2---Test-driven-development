c5932cad4287add90b7e072652831f4d5bd86021
Nahed Al Awlaki <Nahed.atik@hotmail.com>
2023-02-28 12:41:56 +0100

    Player class updated

--- Classes/player.py
+++ Classes/player.py
@@ -1,2 +1,24 @@
 class Player:
-    pass
+
+    def __init__(self, name, score, currentScore):
+        self._name = name
+        self._score = score
+        self._currentScore = currentScore
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_name(self):
+        return self._name
+
+    def set_score(self, new_score):
+        self._score = new_score
+
+    def get_score(self):
+        return self._score
+
+    def set_tempScore(self, score):
+        self._currentScore = score
+
+    def get_tempScore(self):
+        return self._currentScore
