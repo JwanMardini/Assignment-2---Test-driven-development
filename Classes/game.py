6d12a048bd17e7f0cae907c38380844b3288a9fa
JwanMardini <jwan.99.mardini@gmail.com>
2023-03-04 23:33:24 +0100

    Fixed some bugs in main

--- Classes/game.py
+++ Classes/game.py
@@ -4,8 +4,8 @@ class Game:
         self._header = "***************************\n" + "--- Welcome to pig dice ---\n" + "***************************\n"
         self._turn = 1
         self._startMenu = "Enter 1 for one player game\nEnter 2 for two players game"
-        self._option_menu = '''Press 1 to change name\n
-Press 2 to quit game\n'''
+        self._option_menu = '''Press 1 to change name
+Press 2 to quit game\nPress 3 to roll\n'''
         self._difficultyMenu = '''Enter (e) for easy mode and (h) for hard mode: '''
 
     def header(self):
@@ -32,11 +32,11 @@ Press 2 to quit game\n'''
     def get_defficultyMenu(self):
         return self._difficultyMenu
 
-    def ingameMenu(self, choice, player):
-        if choice == 1:
-            player_name = input("Type in the new name: ")
-            player.set_name(player_name)
-        elif choice == 2:
-            exit()
-        else:
-            pass
+    # def ingameMenu(self, choice, player):
+    #     if choice == 1:
+    #         player_name = input("Type in the new name: ")
+    #         player.set_name(player_name)
+    #     elif choice == 2:
+    #         exit()
+    #     else:
+    #         pass
