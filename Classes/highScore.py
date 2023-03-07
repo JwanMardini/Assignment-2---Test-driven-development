ea60ebdf703155b20b26ecfd2448b08c2c19f02a
JwanMardini <jwan.99.mardini@gmail.com>
2023-03-07 15:15:55 +0100

    Modified import statments, so we can run the test

--- Classes/highScore.py
+++ Classes/highScore.py
@@ -1,7 +1,7 @@
 class HighScore:
 
     def __init__(self) -> None:
-        self._highScore = 10
+        self._highScore = 100
 
     def set_highScore(self, new_highScore):
         self._highScore = new_highScore
